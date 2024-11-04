91185516d060b90f1de3a636ba9ed4eb0ecb9ab8
ritika-du <ritika@defenseunicorns.com>
2024-11-03 22:31:12 -0500

    chore: update testing

---
+++ tests/bitbucket.test.ts
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2024 Defense Unicorns
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+ */
+
+import { test, expect } from "@playwright/test";
+import path from "path";
+
+function randomProjectName() {
+  return `uds-package-bitbucket-${Math.floor(Math.random() * 1000)}`;
+}
+
+test("setup a project", async ({ page }) => {
+  const projectName = randomProjectName();
+  const projectKey = projectName.toUpperCase();
+
+  await page.goto("/projects?create");
+  await page.getByLabel("Project name").fill(projectName);
+  await page.getByLabel("Project key").fill(projectKey);
+  await page.getByLabel("Description").fill("This is a test project");
+  await page.getByRole("button", { name: "Change avatar" }).click();
+  const fileChooserPromise = page.waitForEvent("filechooser");
+  await page.getByRole("button", { name: "Upload an image" }).click();
+  const fileChooser = await fileChooserPromise;
+  await fileChooser.setFiles(path.join(__dirname, "data/unicorns.jpeg"));
+  await page.getByRole("button", { name: "Done" }).click();
+  await page.getByRole("button", { name: "Create project" }).click();
+
+  await expect(page).toHaveURL(`/projects/${projectKey}`);
+
+  await test.step("create a repository in project", async () => {
+    await page.goto(`/projects/${projectKey}/repos?create`);
+
+    await page.getByLabel("Name(required)").fill(projectName);
+    await page.getByLabel("Description").fill("This is a test repository");
+    await page.getByRole("button", { name: "Create repository" }).click();
+
+    await expect(page).toHaveURL(
+      `/projects/${projectKey}/repos/${projectName}/browse`
+    );
+  });
+});
+
+test("test opensearch connection", async ({ page }) => {
+  await page.goto("/admin/server-settings");
+  await page.locator("#testConnectionButton").click();
+
+  await expect(page.locator('#testResultMessage')).toHaveClass("success");
+});
